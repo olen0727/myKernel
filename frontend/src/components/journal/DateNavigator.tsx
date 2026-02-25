fd6cc2f7b389ebec5f5664d4624431105c7f40d7
olen <olen0727@gmail.com>
2026-02-25 22:24:43 +0800

    日期格式調整

--- frontend/src/components/journal/DateNavigator.tsx
+++ frontend/src/components/journal/DateNavigator.tsx
@@ -63,7 +63,7 @@ export function DateNavigator({ currentDate, onDateChange, className }: DateNavi
                         )}
                     >
                         <CalendarIcon className="mr-2 h-4 w-4" />
-                        {currentDate ? format(currentDate, "PPP") : <span>Pick a date</span>}
+                        {currentDate ? format(currentDate, "yyyy-MM-dd") : <span>Pick a date</span>}
                     </Button>
                 </PopoverTrigger>
                 <PopoverContent className="w-auto p-0" align="start">
